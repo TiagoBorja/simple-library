b74bd44342f3dad8e6ac0b0da70650f2033c4bf8
TiagoBorja <borjatiago05@gmail.com>
2024-10-08 20:23:16 +0100

    Initial Commit

---
+++ php/livros.php
@@ -0,0 +1,30 @@
+<div class="row">
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title mb-0">Livros</h5>
+        </div>
+        <div class="table-responsive">
+            <table class="table">
+                <thead class="thead-light">
+                    <tr>
+                        <th></th>
+                        <th scope="col">Título</th>
+                        <th scope="col">ISBN</th>
+                        <th scope="col">Autor</th>
+                        <th scope="col">Ano de Publicação</th>
+                        <th scope="col">Descrição</th>
+                    </tr>
+                </thead>
+                <tbody class="customtable">
+                    <tr>
+                        <th>
+                            <input type="radio" class="form-check-input" id="customControlValidation1"
+                                name="radio-stacked" />
+                        </th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+</div>
